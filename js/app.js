68616246fb1a780ce45108936bd1a2801c6be9c9
Bhushan <bhushan.patil@objectedge.com>
2019-06-24 14:56:19 +0530

    updated optimized logic for routing

--- js/app.js
+++ js/app.js
@@ -1,70 +1,81 @@
-;(function($) {
-function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
+;(function($){
 
+    $(document).ready(function(){
+
+function viewModel() {
     var self = this;
-    //self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
-    self.pages = ['Home', 'About', 'Products'];
     self.chosenPageId = ko.observable();
-    self.chosenFolderData = ko.observable();
-    self.chosenMailData = ko.observable();
-    self.pageData = ko.observable();
+    self.pages = ko.observableArray(["Home", "About", "Product"]);
     self.template = ko.observable();
-
-    // Behaviours    
-    self.goToPage = function(page) { location.hash = page };
-    self.goToProduct = function(productid) { location.hash = 'Products/' + productid };
-
-   
-        // Client-side routes    
-    Sammy(function() {
-        this.get('#Home', function() {
-            self.template("page-template");
-            //get content for home page 
-            self.chosenPageId("Home");
-            $.get("/server/home.json",function(data){
-                self.pageData(data);
-            });
-        });
-        this.get('#About', function() {
-            self.template("page-template");
-            //get content for home page 
-            self.chosenPageId("About");
-            $.get("/server/about.json",function(data){
-                self.pageData(data);
-            });
-        });
-
-        this.get('#Products', function() {
-            self.template("product-template");
-            //get content for home page 
-            self.chosenPageId("Products");
-            // $.get("/server/about.json",function(data){
-            //     self.pageData(data);
-            // });
-        });
-
-        this.get('#Products/:pageid', function() {
-            console.log(this.params.pageid)
-            self.chosenPageId(this.params.pageid);
-            self.chosenMailData(null);
-            //$.get("/mail", { folder: this.params.folder }, self.chosenFolderData);
-        });
-
-        // this.get('#:folder/:mailId', function() {
-        //     self.chosenPageId(this.params.folder);
-        //     self.chosenFolderData(null);
-        //     //$.get("/mail", { mailId: this.params.mailId }, self.chosenMailData);
-        // });
-    
-        this.get('', function() { this.app.runRoute('get', '#Home') });
-    }).run(); 
-    
-    
-
+    self.goToPage = function (page){
+        location.hash = page;
+    }
+    self.goToProduct = function (productid){
+        location.hash = 'Product/'+ productid;
+    }
 }
+function pageViewModel() {
+    var self = this;
+    self.name = ko.observable('Home');
+    self.content = ko.observable('Home default content')
+}
+// function aboutViewModel() {
+//     var self = this;
+//     self.name = 'About';
+// }
+function productViewModel() {
+    var self = this;
+    self.title = 'Product';
+    self.productid = 1;
+    self.skuid = 1;
+}
+var vm = {
+    Main: new viewModel(),
+    Page: new pageViewModel(),
+    Product: new productViewModel
+};
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+Sammy(function () {
+        this.get('#Home', function () {
+            vm.Main.chosenPageId(this.params.page);     
+            vm.Main.template("page-template")
+            vm.Page.name("Home")
+            $.get('./server/home.json',function(data){
+                vm.Page.name(data.title);
+                vm.Page.content(data.content);
+            })
+        });
+        this.get('#About', function () {
+            vm.Main.chosenPageId(this.params.page);     
+            vm.Main.template("page-template")
+            //vm.Page.name("About")
+            $.get('./server/about.json',function(data){
+                vm.Page.name(data.title);
+                vm.Page.content(data.content);
+            })
+        });
+        this.get('#Product', function () {
+            vm.Main.chosenPageId(this.params.page);     
+            vm.Main.template("product-template")
+            //vm.Page.name("About")
+            //fetch product details here
+        });
+
+        this.get('#Product/:productid', function () {
+            vm.Main.chosenPageId("Product");     
+            vm.Main.template("product-template")
+            //this.params.productid
+            //vm.Page.name("About")
+            //fetch product details here
+        });
+
+        // this.get('#:page', function () {
+        //     vm.Main.chosenPageId(this.params.page);     
+        //     vm.Main.template("page-template") 
+        // });
+    }).run('#Home');
+ko.applyBindings(vm);
+
+});
+
 })(jQuery);
