561e8185697e55dc8b5946eb5b9018b633209288
Rakshanda Wankhede <rakshanda.wankhede@objectedge.com>
2019-07-11 14:11:37 +0530

    Code Optimization Completed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 ;(function($){
 
-$(document).ready(function(){
-       
+$(document).ready(function(){    
   //-------------------------------------Code For Fascets Collapsing -------------------------------         
-$(document).on('click', '.panel-heading.clickable', function(e){
-    
+$(document).on('click', '.panel-heading.clickable', function(e){ 
     var $this = $(this);
 	if(!$this.hasClass('panel-collapsed')) {
 		$this.parents('.panel').find('.panel-body').slideDown();
@@ -37,10 +35,6 @@ function pageViewModel() {
     self.name = ko.observable('Home');
     self.content = ko.observable('Home default content')
 }
-// function aboutViewModel() {
-//     var self = this;
-//     self.name = 'About';
-// }
 //-----------------------------------------------------------------------------------------------
 function productViewModel() {
             //---------------------------------Observable and Observable Array declarations---------
@@ -58,89 +52,36 @@ function productViewModel() {
             self.MinVal =ko.observable(200);
             self.MaxVal=ko.observable(700);
             self.FascetLimit =ko.observable(5);
+            self.totalCategory =ko.observableArray();
+            self.RatingArray=ko.observableArray();
             self.color_select = ko.observableArray();
             self.size_select = ko.observableArray();
             self.brand_select = ko.observableArray();
             self.rating_select = ko.observableArray();
-            //-----------------------------------------Price Filter------------------------------------------------------------------
-            self.Pricefilter =ko.computed(function(){
-                if(self.size_select().length != 0 || self.brand_select().length != 0 || self.color_select().length != 0 || self.rating_select().length != 0 )
-                var clonedArr = $.extend(true, [], self.Products());
-                else
-                var clonedArr = $.extend(true, [], self.allProduct());
-                var temp =[];
-                
-                clonedArr.map(function(val){
-                    if(val.Sku[0].price <= self.SecondRange() && val.Sku[0].price >= self.fisrtRange())
-                        temp.push(val);
-                });
-                    self.Products(temp);
-            });
-
             //-------------------------------------------Sort functions--------------------------------
             self.SortByAsecnding = function(){
                 $(".a1").css({"font-weight":"normal"});$(".a2").css({"font-weight":"bold"});$(".a3").css({"font-weight":"normal"});
                 var clonedArr = $.extend(true, [], this.Products());
                 clonedArr.sort((a,b)=>(a.Sku[0].price <b.Sku[0].price)?-1:((a.Sku[0].price > b.Sku[0].price)? 1 : 0));
+                self.allProduct().sort((a,b)=>(a.Sku[0].price <b.Sku[0].price)?-1:((a.Sku[0].price > b.Sku[0].price)? 1 : 0));
                 this.Products(clonedArr);
             };
             self.SortByDesecnding = function(){
-                $(".a2").css({"font-weight":"normal"});$(".a2").css({"font-weight":"normal"});$(".a3").css({"font-weight":"bold"});
+                $(".a1").css({"font-weight":"normal"});$(".a2").css({"font-weight":"normal"});$(".a3").css({"font-weight":"bold"});
                 var clonedArr = $.extend(true, [], this.Products());
                 clonedArr.sort((a,b)=>(a.Sku[0].price >b.Sku[0].price)?-1:((a.Sku[0].price < b.Sku[0].price)? 1 : 0));
+                self.allProduct().sort((a,b)=>(a.Sku[0].price >b.Sku[0].price)?-1:((a.Sku[0].price < b.Sku[0].price)? 1 : 0));
                 this.Products(clonedArr);
             };
             self.SortByPopularity = function(){
                 $(".a1").css({"font-weight":"bold"});$(".a2").css({"font-weight":"normal"});$(".a3").css({"font-weight":"normal"});
                 var clonedArr = $.extend(true, [], this.Products());
                 clonedArr.sort((a,b)=>(a.Sku[0].rating >b.Sku[0].rating)?-1:((a.Sku[0].rating < b.Sku[0].rating)? 1 : 0));
+                self.allProduct().sort((a,b)=>(a.Sku[0].rating >b.Sku[0].rating)?-1:((a.Sku[0].rating < b.Sku[0].rating)? 1 : 0));
                 this.Products(clonedArr);
-
             };
-            //-----------------------------------------------Fascets Coding---------------------------------------
-            self.applyFilter = function(Fascets_Array){
-            
-                var clonedArr = $.extend(true, [], this.allProduct());
-                var temp=[];
-                for (var fascet in Fascets_Array){
-                    Fascets_Array[fascet].map(function(SelectedValue){
-                        clonedArr.map(function(CurrentValue){
-                            if(fascet==="size"){
-                                if ($.inArray(SelectedValue,CurrentValue.size) != -1){
-                                    temp.push(CurrentValue);
-                                }
-                            }
-                            if(fascet==="color"){
-                                if ($.inArray(SelectedValue,CurrentValue.color) != -1){
-                                    temp.push(CurrentValue);
-                                }
-                            }
-                            if(fascet==="brand"){
-                                if ($.inArray(SelectedValue,CurrentValue.brand) != -1){
-                                    temp.push(CurrentValue);
-                                }
-                            }
-                            if(fascet==="rating"){
-                                if (SelectedValue <= CurrentValue.rating[0]){
-                                    temp.push(CurrentValue);
-                                }
-                            }
-                        });
-                    });
-                    clonedArr=temp;
-                    temp=[];
-                }
-                var another_ar =[];
-                clonedArr.map(function(el)
-                {
-                    if($.inArray(el, another_ar) === -1) another_ar.push(el);
-                });
-                    self.Products(another_ar);
-            };
-        
+            //-----------------------------------------------Fascets Coding---------------------------------------          
             self.All_Compute_observe = ko.computed(function(){
-           // console.log(self.size_select(),self.brand_select(),self.color_select());
-                var Fascets_Array =[];
                             if(self.size_select().length > 1)
                             {
                                 self.size_select().shift();
@@ -149,25 +90,71 @@ function productViewModel() {
                             {
                                 self.rating_select().shift();
                             }
-                            if (self.size_select().length != 0){
-                                Fascets_Array["size"]=self.size_select() ;
+                        var clonedArr = $.extend(true, [], self.allProduct());
+                        var temp=[];
+                        clonedArr.map(function(val){
+                            if(val.Sku[0].price <= self.SecondRange() && val.Sku[0].price >= self.fisrtRange())
+                                temp.push(val);
+                        });
+                            clonedArr=temp;
+                            temp=[];
+                        if (self.size_select().length != 0){
+                            clonedArr.map(function(CurrentValue){
+                            var prop=CurrentValue.size;
+                            var result = self.size_select().filter(function(item) {
+                                return prop.includes(item); 
+                            })
+                            if(result.length!=0)
+                                temp.push(CurrentValue);
+                        });
+                            clonedArr=temp;
+                            temp=[];
                             }
-                            if (self.brand_select().length != 0){
-                                Fascets_Array["brand"]=self.brand_select();
+                        if (self.brand_select().length != 0){
+                            clonedArr.map(function(CurrentValue){
+                            var prop=CurrentValue.brand;
+                            var result = self.brand_select().filter(function(item) {
+                                return prop.includes(item); 
+                            })
+                            if(result.length!=0)
+                            temp.push(CurrentValue);
+                        });
+                            clonedArr=temp;
+                            temp=[];
                             }
-                            if (self.color_select().length != 0){
-                                Fascets_Array["color"]=self.color_select();
+                        if (self.color_select().length != 0){
+                            clonedArr.map(function(CurrentValue){
+                            var prop=CurrentValue.color;
+                            var result = self.color_select().filter(function(item) {
+                                return prop.includes(item); 
+                            })
+                            if(result.length!=0)
+                            temp.push(CurrentValue);
+                        });
+                            clonedArr=temp;
+                            temp=[];
                             }
-                            if (self.rating_select().length != 0){
-                                Fascets_Array["rating"]=self.rating_select();
-                            }
-                            if (!jQuery.isEmptyObject(Fascets_Array)){
-                                self.applyFilter(Fascets_Array);
-                            }   
-                            else{
-                                self.Products(self.allProduct());
-                            }
-                } );
+                        if (self.rating_select().length != 0){
+                            clonedArr.map(function(CurrentValue){
+                                self.rating_select().filter(function(item) {
+                                if (item <= CurrentValue.rating[0]){
+                                    temp.push(CurrentValue);
+                                }
+                            });
+                        });
+                            clonedArr=temp;
+                            temp=[];
+                        }   
+                        if(self.size_select().length == 0 && 
+                            self.brand_select().length == 0 && 
+                            self.color_select().length == 0 && 
+                            self.rating_select().length == 0 && 
+                            self.fisrtRange()== self.MinVal() && 
+                            self.SecondRange()== self.MaxVal())
+                            self.Products(self.allProduct());
+                        else
+                            self.Products(clonedArr); 
+                     });
         //-------------------------------------------------------------------Fascet Coding End----------------
                     //-----------------------------Use of flags to show fascet's less and more functionality--------
             self.brandFlagFun = function (){
@@ -206,7 +193,6 @@ function productViewModel() {
                 $("#"+size).html("Size:"+arr+" ");
                 $("#"+pri_id).html(" "+pri);
                 $("#"+product.Sku[0].prodId).attr("src",img);
-                // console.log(arr);
             }
             self.changeColor = function(product){
                 var color_span =product.Sku[0].prodId+"heart";
@@ -223,16 +209,12 @@ var vm = {
     Page: new pageViewModel(),
     Product: new productViewModel
 };
-
 Sammy(function () {
         this.get('#Home', function () {
             vm.Main.chosenPageId(this.params.page);  
             vm.Main.template("page-template")
             vm.Page.name("Home")
-            // $.get('./server/home.json',function(data){
-            //     vm.Page.name(data.title);
-            //     vm.Page.content(data.content);
-            // })
+            
         });
 //--------------------------------------PLP Page Array---------------------
         this.get('#About', function () {
@@ -243,6 +225,8 @@ Sammy(function () {
                 var totalSize =[];
                 var totalColor =[];
                 var totalBrand =[];
+                var TotalCategory=[];
+                var TotalRating=[];
                
                 var clonedArr = data["items"];
                 clonedArr.sort((a,b)=>(a.price <b.price)?-1:((a.price > b.price)? 1 : 0));
@@ -297,6 +281,15 @@ Sammy(function () {
                             {
                                 totalBrand.push(v.brand);
                             }
+                            if($.inArray(v.category, TotalCategory) === -1) 
+                            {
+                                TotalCategory.push(v.category);
+                            }
+                            var x =Math.floor(v.rating);
+                            if($.inArray(x, TotalRating) === -1) 
+                            {
+                                TotalRating.push(x);
+                            }
                         });
                     vm.Product.Products(unique_prod_arr); 
                     vm.Product.allProduct(unique_prod_arr); 
@@ -306,33 +299,19 @@ Sammy(function () {
                     vm.Product.sizeObserve(totalSize);
                     vm.Product.colorObserve(totalColor);
                     vm.Product.brandObserve(totalBrand);  
+                    vm.Product.totalCategory(TotalCategory);
+                    vm.Product.RatingArray(TotalRating);
                 });
-            //vm.Page.name("About")
-            // $.get('./server/about.json',function(data){
-            //     vm.Page.name(data.title);
-            //     vm.Page.content(data.content);
-            // })
         });
-
         this.get('#Product', function () {
             vm.Main.chosenPageId(this.params.page);     
             vm.Main.template("product-template")
-            //vm.Page.name("About")
-            //fetch product details here
         });
 
         this.get('#Product/:productid', function () {
             vm.Main.chosenPageId("Product");     
             vm.Main.template("product-template")
-            //this.params.productid
-            //vm.Page.name("About")
-            //fetch product details here
         });
-
-        // this.get('#:page', function () {
-        //     vm.Main.chosenPageId(this.params.page);     
-        //     vm.Main.template("page-template") 
-        // });
     }).run('#Home');
 
 ko.applyBindings(vm);
@@ -347,7 +326,6 @@ var filterfun = function(){
         $("#viewSort").removeClass('showsortBy').addClass('hidesortBy');
     }
 }
-
 var sortfun = function(){
         $("#viewSort").toggleClass('hidesortBy showsortBy');
         $("#viewFilter").removeClass('showfilter').addClass('hidefilter');
