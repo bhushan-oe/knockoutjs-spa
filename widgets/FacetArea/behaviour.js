40bf8816d03d92f92a19054de4dcc9a2422028fe
Deepak Verma <deepak.verma@foodappetency.com>
2019-07-11 14:26:40 +0530

    category, brand filters and sortby  changes

--- widgets/FacetArea/behaviour.js
+++ widgets/FacetArea/behaviour.js
@@ -1,5 +1,24 @@
 console.log("loading FacetArea behaviour");
 //performLateBinding(viewModel, 'filter-area');
-performLateBinding(productsViewModel, 'filter');
-performLateBinding(productsViewModel, 'brand-facets');
+performLateBinding(listingViewModel, 'filter');
+performLateBinding(listingViewModel, 'brand-facets');
+performLateBinding(listingViewModel, 'category-facets');
+performLateBinding(listingViewModel, 'price-facet');
+performLateBinding(listingViewModel, 'color-facets');
 //performLateBindingByClass(productsViewModel, 'brand-checkbox');
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
